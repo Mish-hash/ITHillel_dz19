0597dee9d39d11af83a4da1fa880b3be1869583e
mish-hash <poluektov23@gmail.com>
2022-08-28 16:15:27 +0300

    Created component Header and Welcome

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import Welcome from "./Welcome";
+
+
+function Header() {
+    return (
+        <header>
+            <Welcome />
+            <Welcome />
+            <Welcome />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Welcome.js
@@ -0,0 +1,8 @@
+
+function Welcome() {
+    return (
+        <div><h3>Welcome UserName</h3></div>
+    );
+}
+
+export default Welcome;
